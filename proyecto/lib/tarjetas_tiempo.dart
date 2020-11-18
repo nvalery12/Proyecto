ce61c75dbb06982a1523772aa1bc78a6087b644d
Diego <diego.gamboaa03@gmail.com>
2020-11-18 02:48:16 -0400

    Color de fondo

---
+++ proyecto/lib/tarjetas_tiempo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TarjetasTiempo extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _TarjetasTiempo();
+  }
+  
+}
+
+class _TarjetasTiempo extends State<TarjetasTiempo>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
