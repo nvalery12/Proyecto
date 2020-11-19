839eab5a61037f534a67e4885ea7cb563c9e236d
Diego <diego.gamboaa03@gmail.com>
2020-11-19 01:14:57 -0400

    Rectangulo de reloj, ahora si xd
    Un pequeño error

--- proyecto/lib/main.dart
+++ proyecto/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 var backgroundColors = [0xffec524b,0xfff5b461,0xfff3eac2];
@@ -26,17 +27,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
   return Scaffold(
-    body: Center(
-      child: IconButton(
-        icon: Icon(Icons.access_alarm),
-        onPressed: (){
-          setState(() {
-            state++;
-            if(state == 3)
-              state = 0;
-          });
-        },
-      ),
+    body: Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.bottomCenter,  //Alineo el hijo al centro abajo
+          child: Container(
+            height: (MediaQuery.of(context).size.height)-300,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
+        Center(
+          child: IconButton(
+              icon: Icon(Icons.access_alarm),
+          onPressed: (){ //Cuando presiono
+            setState(() { // Setea el estado
+              state++;
+              if(state == 3)
+                state = 0;
+            });
+          },
+        ),
+          )
+      ]
     ),
     backgroundColor: Color(backgroundColors[state]),
   );
