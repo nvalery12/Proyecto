ff7ef6746686adddaa8a38a2f8b94834b4c8760c
Diego <diego.gamboaa03@gmail.com>
2020-11-20 13:40:46 -0400

    Limpiando y comentando codigo
    Se comento el codigo, y  se pusieron ciertas partes del codigo en funciones

--- proyecto/lib/main.dart
+++ proyecto/lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-var backgroundColors = [0xffec524b,0xfff5b461,0xfff3eac2];
-int state = 0;
-int min = 5, sec = 5;
+var backgroundColors = [0xffec524b,0xfff5b461,0xfff3eac2]; //lista de colores, cada posicion es un color distinto
+int state = 0; //Sirve para llevar un control de la lista de colores
+int min = 5, sec = 5; // minutos y segundos, por ahora esta inicializado, pero cuando este listo se le guardaran los valores que se le pasen
+
+/*Pequeño algoritmo que se encarga del tiempo*/
+void chageTime(){
+  if(sec == 0){
+    sec = 60;
+    min--;
+  }
+  sec--;
+}
+
+/*Algoritmo que se encarga de cambiar colores de fondo*/
+void chageState(){
+  state++;
+  if(state == 3)
+    state = 0;
+}
 void main() {
   runApp(MyApp());
 }
@@ -30,10 +46,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
   return Scaffold(
     body: Stack(
-      children: <Widget>[
+      children: <Widget>[ //Uso stack, porque apilare cosas, una sobre la otra
         Align(
           alignment: Alignment.bottomCenter,  //Alineo el hijo al centro abajo
-          child: Container(
+          child: Container(  //Rectangulo cuadrado blanco
             height: (MediaQuery.of(context).size.height)-300,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
@@ -46,32 +62,33 @@ class MyApp extends StatelessWidget {
           ),
         ),
         Center(
-          child: IconButton(
+          child: IconButton( //Boton de reloj del centro
               icon: Icon(Icons.access_alarm),
               onPressed: (){ //Cuando presiono
-                  setState(() {
-                    min--;
-                    sec--;
+                  setState(() { //Setea el estado, es decir, revisa las variables
+                    chageTime();
+                    chageState();
                   });},
         ),
           ),
         Align(
           child: Container(
-            child: Text(
-              "$min:$sec",
+            child: Text( //Texto de numeros
+              "$min:$sec", 
               style: TextStyle(
-                  fontSize: 40,
+                  fontSize: 90,
                   fontWeight: FontWeight.bold,
                   color: Colors.white
               ),
             ),
             alignment: Alignment.topCenter,
+            padding: EdgeInsets.all(110),
           ),
           alignment: Alignment.topCenter,
         )
       ]
     ),
-    backgroundColor: Color(backgroundColors[state]),
+    backgroundColor: Color(backgroundColors[state]),  //El color se va cambiando dependiendo del state
   );
   }
 
