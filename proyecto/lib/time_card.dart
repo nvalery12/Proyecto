0c2231b2895af33a351e077a08eddf6f6c2b9dbd
Diego <diego.gamboaa03@gmail.com>
2020-11-24 11:40:02 -0400

    Acomodando tarjetas de tiempo

--- proyecto/lib/time_card.dart
+++ proyecto/lib/time_card.dart
@@ -18,35 +18,40 @@ class TimeCard extends StatelessWidget {
         height: (MediaQuery.of(context).size.height)/11,
         child: Row(
           children: <Widget>[
-            Icon(cardIcon,
-              size: 25,
-            ),
             Container(
-              padding: EdgeInsets.only(
-                  left: (MediaQuery.of(context).size.width)/20,
-                  right: (MediaQuery.of(context).size.width)/7
-              ),
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+                child:Icon(cardIcon,
+                  size: 25,
+                )
+            ),
+            Expanded(
+              flex: 1,
+                child:Container(
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
             ),
-            Container(
-              padding: EdgeInsets.only(
-                left: (MediaQuery.of(context).size.width)/11,
-                right: (MediaQuery.of(context).size.width)/12
-              ),
-              child: Text(
-                time,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-              ),
+            Expanded(
+              flex: -1 ,
+                child: Container(
+                  padding: EdgeInsets.only(
+                      left: (MediaQuery.of(context).size.width)/11,
+                      right: (MediaQuery.of(context).size.width)/12
+                  ),
+                  child: Text(
+                    time,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
             ),
-          )
+
         ]
       )
      )
