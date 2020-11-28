24c9572bce42c26586f744cc400f7275b98e374c
Diego <diego.gamboaa03@gmail.com>
2020-11-28 19:17:37 -0400

    Implementando timer en timerPage

--- proyecto/lib/main.dart
+++ proyecto/lib/main.dart
@@ -1,15 +1,11 @@
-import 'dart:collection';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'timerPage.dart';
 
 var backgroundColors = [0xffec524b,0xfff5b461,0xfff3eac2]; //lista de colores, cada posicion es un color distinto
 int state = 0; //Sirve para llevar un control de la lista de colores
 int secTraining = 15, minTraining = 0, secRest=15, minRest=0, sets=2, exercises = 2; // minutos, segundos, secundos de descanso y sets. Por ahora esta inicializado, pero cuando este listo se le guardaran los valores que se le pasen
 String secText, minText;
-bool isTimerActive = false;
 var seconds, minutes;
 
 void main() {
@@ -32,62 +28,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var timerQueue = List<Duration>();
-  Timer currentTimer;
 
-//Create the sets
-  void startSets() {
-    timerQueue.add(Duration(seconds: 10));
-    for (var i = 0; i < sets; i++) {
-      for (var j = 0; j < exercises; j++) {
-        timerQueue.add(Duration(seconds: 15));
-      }
-      timerQueue.add(Duration(seconds: 12));
-    }
-    startNextTimer();
-  }
-
-  void stopTimer() {
-    currentTimer.cancel();
-    currentTimer = null;
-    timerQueue.insert(0,Duration(seconds: seconds,minutes: minutes));
-  }
-
-  void startNextTimer() {
-    if (timerQueue.isEmpty) {
-      return;
-    }
-
-    var duration = timerQueue.first;
-    timerQueue.remove(timerQueue.first);
-    seconds = duration.inSeconds % 60;
-    minutes = duration.inMinutes;
-
-    currentTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (seconds > 0){
-          seconds--;
-        } else if (seconds == 0 && minutes > 0) {
-          minutes--;
-          seconds = 59;
-        } else {
-          currentTimer.cancel();
-          currentTimer = null;
-        }
-        minText = minToString(minutes);
-        secText = secToString(seconds);
-        if (currentTimer == null) {
-          startNextTimer();
-        }
-      });
+  /*Algoritmo que se encarga de cambiar colores de fondo*/
+  chageState(){
+    setState(() {
+      state++;
+      if(state == 3)
+        state = 0;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    /*minText=minToString(minTraining);
-    secText=secToString(secTraining);*/
     return Scaffold(
-      body: Timer_Page(parameterFuction: chageState),
+      body: Timer_Page(changeState: chageState),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -108,32 +62,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       backgroundColor: Color(backgroundColors[state]),  //El color se va cambiando dependiendo del state
     );
-  }
-}
-
-/*Algoritmo que se encarga de cambiar colores de fondo*/
-void chageState(){
-  state++;
-  if(state == 3)
-    state = 0;
-}
-
-//Convierte el segundo de entero a String
-String secToString(int seconds){
-  String num = seconds.toString();
-  if(seconds>9){
-    return num;
-  }else{
-    return "0$num";
-  }
-}
-
-//Convierte el minuto de entero a String
-String minToString(int minutes){
-  String num = minutes.toString();
-  if(minutes>9){
-    return num;
-  }else{
-    return "0$num";
   }
 }
--- proyecto/lib/timerPage.dart
+++ proyecto/lib/timerPage.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'time_card_list.dart';
 
+int secTraining = 15, minTraining = 0, secRest=15, minRest=0, sets=2, exercises = 2; // minutos, segundos, secundos de descanso y sets. Por ahora esta inicializado, pero cuando este listo se le guardaran los valores que se le pasen
+String secText, minText;
+var seconds, minutes;
+
 
 class Timer_Page extends StatefulWidget{
-  final parameterFuction;
-  const Timer_Page({Key key,@required this.parameterFuction}) : super(key: key);
+  final changeState;
+  const Timer_Page({Key key, this.changeState}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -14,6 +20,57 @@ class Timer_Page extends StatefulWidget{
 }
 
 class _Timer_Page extends State<Timer_Page>{
+  bool isTimerActive = false;
+  var timerQueue = List<Duration>();
+  Timer currentTimer;
+
+  //Create the sets
+  void startSets() {
+    timerQueue.add(Duration(seconds: 10));
+    for (var i = 0; i < sets; i++) {
+      for (var j = 0; j < exercises; j++) {
+        timerQueue.add(Duration(seconds: 15));
+      }
+      timerQueue.add(Duration(seconds: 12));
+    }
+    startNextTimer();
+  }
+
+  void stopTimer() {
+    currentTimer.cancel();
+    currentTimer = null;
+    timerQueue.insert(0,Duration(seconds: seconds,minutes: minutes));
+  }
+
+  void startNextTimer() {
+    if (timerQueue.isEmpty) {
+      return;
+    }
+
+    var duration = timerQueue.first;
+    timerQueue.remove(timerQueue.first);
+    seconds = duration.inSeconds % 60;
+    minutes = duration.inMinutes;
+
+    currentTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (seconds > 0){
+          seconds--;
+        } else if (seconds == 0 && minutes > 0) {
+          minutes--;
+          seconds = 59;
+        } else {
+          currentTimer.cancel();
+          currentTimer = null;
+        }
+        minText = minToString(minutes);
+        secText = secToString(seconds);
+        if (currentTimer == null) {
+          startNextTimer();
+        }
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -54,8 +111,18 @@ class _Timer_Page extends State<Timer_Page>{
               ),
               iconSize: 125,
                 onPressed: (){ //Cuando presiono
-                    this.widget.parameterFuction();
-                    },
+                  if(isTimerActive == false){
+                    isTimerActive = true;
+                    if(timerQueue.isEmpty){
+                      startSets();
+                    }else{
+                      startNextTimer();
+                    }
+                  }else{
+                    isTimerActive = false;
+                    stopTimer();
+                  }
+                },
             ),
           ),
           Align(
@@ -64,5 +131,25 @@ class _Timer_Page extends State<Timer_Page>{
           )
         ]
     );
+  }
+}
+
+//Convierte el segundo de entero a String
+String secToString(int seconds){
+  String num = seconds.toString();
+  if(seconds>9){
+    return num;
+  }else{
+    return "0$num";
+  }
+}
+
+//Convierte el minuto de entero a String
+String minToString(int minutes){
+  String num = minutes.toString();
+  if(minutes>9){
+    return num;
+  }else{
+    return "0$num";
   }
 }
