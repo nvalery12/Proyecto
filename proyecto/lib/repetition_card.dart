3294ce69fdc772fa3f456ddfa2263ed69a3555f4
Yara Buassaf <natalybuassaf27@gmail.com>
2020-12-06 06:09:47 -0400

    conPadding

--- proyecto/lib/repetition_card.dart
+++ proyecto/lib/repetition_card.dart
@@ -16,72 +16,75 @@ class RepetitionCard extends StatefulWidget {
 class _RepetitionCardState extends State<RepetitionCard> {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-        onPressed: () {
-          Picker(
-            adapter: NumberPickerAdapter(data: <NumberPickerColumn>[
-              const NumberPickerColumn(begin: 0, end: 30,),
-            ]),
-            hideHeader: true,
-            confirmText: 'OK',
-            confirmTextStyle: TextStyle(inherit: false, color: Color(0xffec524b), fontSize: 22),
-            title: const Text('Select '),
-            selectedTextStyle: TextStyle(color: Color(0xff16697a)),
-            onConfirm: (Picker picker, List<int> value) {
-              setState(() {
-                  widget.repetition= picker.getSelectedValues()[0];
-                  print("${widget.repetition}" + " ${widget.title} en repetition Card");
-                  widget.function(widget.repetition);
-              });
-            },
-          ).showDialog(context);
-        },
-        color: Color(0xfff8f1f1),   //color de repetition card
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)
-        ),
-        textColor: Color(0xff16697a),
-        child:Container(
-            width: (MediaQuery.of(context).size.width)/1.25,
-            height: (MediaQuery.of(context).size.height)/13,
-            child: Row(
-                children: <Widget>[
-                  Container(
-                      child:Icon(widget.cardIcon,
-                        size: 25,
-                      )
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child:Container(
-                      child: Text(
-                        widget.title,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+    return Padding(                         //Padding de las cards
+      padding: const EdgeInsets.all(2.0),
+      child: RaisedButton(
+          onPressed: () {
+            Picker(
+              adapter: NumberPickerAdapter(data: <NumberPickerColumn>[
+                const NumberPickerColumn(begin: 0, end: 30,),
+              ]),
+              hideHeader: true,
+              confirmText: 'OK',
+              confirmTextStyle: TextStyle(inherit: false, color: Color(0xffec524b), fontSize: 22),
+              title: const Text('Select '),
+              selectedTextStyle: TextStyle(color: Color(0xff16697a)),
+              onConfirm: (Picker picker, List<int> value) {
+                setState(() {
+                    widget.repetition= picker.getSelectedValues()[0];
+                    print("${widget.repetition}" + " ${widget.title} en repetition Card");
+                    widget.function(widget.repetition);
+                });
+              },
+            ).showDialog(context);
+          },
+          color: Color(0xfff8f1f1),   //color de repetition card
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)
+          ),
+          textColor: Color(0xff16697a),
+          child:Container(
+              width: (MediaQuery.of(context).size.width)/1.25,
+              height: (MediaQuery.of(context).size.height)/13,
+              child: Row(
+                  children: <Widget>[
+                    Container(
+                        child:Icon(widget.cardIcon,
+                          size: 25,
+                        )
                     ),
-                  ),
-                  Expanded(
-                      flex: -1 ,
-                      child: Container(
-                        padding: EdgeInsets.only(
-                            left: (MediaQuery.of(context).size.width)/11,
-                            right: (MediaQuery.of(context).size.width)/12
-                        ),
-                        child:Text(
-                          "${widget.repetition}X",
+                    Expanded(
+                      flex: 1,
+                      child:Container(
+                        child: Text(
+                          widget.title,
                           style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      )
-                  ),
+                      ),
+                    ),
+                    Expanded(
+                        flex: -1 ,
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              left: (MediaQuery.of(context).size.width)/11,
+                              right: (MediaQuery.of(context).size.width)/12
+                          ),
+                          child:Text(
+                            "${widget.repetition}X",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                    ),
 
-                ]
-            )
-        )
+                  ]
+              )
+          )
+      ),
     );
   }
 }
--- proyecto/lib/time_card.dart
+++ proyecto/lib/time_card.dart
@@ -16,92 +16,95 @@ class TimeCard extends StatefulWidget {
 class _TimeCardState extends State<TimeCard> {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton( ////aquiiiiiiiii
-      onPressed: () {
-        Picker(
-          adapter: NumberPickerAdapter(data: <NumberPickerColumn>[
-            const NumberPickerColumn(begin: 0, end: 5, ),
-            const NumberPickerColumn(begin: 0, end: 59, jump: 1),
-          ]),
-          delimiter: <PickerDelimiter>[
-            PickerDelimiter(
-              child: Container(
-                width: 30.0,
-                alignment: Alignment.center,
-                child: Text(":"),
-              ),
-            )
-          ],
-          hideHeader: true,
-          confirmText: 'OK',
-          confirmTextStyle: TextStyle(inherit: false, color: Color(0xffec524b), fontSize: 22),
-          title: const Text('Seleccionar duración'),
-          selectedTextStyle: TextStyle(color: Color(0xff16697a)),
-          onConfirm: (Picker picker, List<int> value) {
-            Duration _duration = Duration(minutes: picker.getSelectedValues()[0], seconds: picker.getSelectedValues()[1]);
-            setState(() {
-              if( ((_duration.inSeconds % 60) > 2) && ((_duration.inMinutes) == 0) ){
-                widget.sec = _duration.inSeconds % 60;
-                widget.min = _duration.inMinutes;
-                widget.function(widget.sec,widget.min);
-              }else{
-                final scaffold = Scaffold.of(context);
-                scaffold.showSnackBar(
-                  SnackBar(
-                    content: const Text('Valor invalido!'),
-                  ),
-                );
-              }
-            });
-          },
-        ).showDialog(context);
-      },
-      color: Color(0xfff8f1f1), //color de time cards
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)
-        ),
-      textColor: Color(0xff16697a),
-      child:Container(
-        width: (MediaQuery.of(context).size.width)/1.25,
-        height: (MediaQuery.of(context).size.height)/13,
-        child: Row(
-          children: <Widget>[
-            Container(
-                child:Icon(widget.cardIcon,
-                  size: 25,
-                )
-            ),
-            Expanded(
-              flex: 1,
-                child:Container(
-                  child: Text(
-                    widget.title,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-            ),
-            Expanded(
-              flex: -1 ,
+    return Padding(                         //Padding de las cards
+      padding: const EdgeInsets.all(2.0),
+      child: RaisedButton(
+        onPressed: () {
+          Picker(
+            adapter: NumberPickerAdapter(data: <NumberPickerColumn>[
+              const NumberPickerColumn(begin: 0, end: 5, ),
+              const NumberPickerColumn(begin: 0, end: 59, jump: 1),
+            ]),
+            delimiter: <PickerDelimiter>[
+              PickerDelimiter(
                 child: Container(
-                  padding: EdgeInsets.only(
-                      left: (MediaQuery.of(context).size.width)/11,
-                      right: (MediaQuery.of(context).size.width)/12
-                  ),
-                  child: Text(
-                    "${secToString(widget.min)}:${secToString(widget.sec)}",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                  width: 30.0,
+                  alignment: Alignment.center,
+                  child: Text(":"),
+                ),
+              )
+            ],
+            hideHeader: true,
+            confirmText: 'OK',
+            confirmTextStyle: TextStyle(inherit: false, color: Color(0xffec524b), fontSize: 22),
+            title: const Text('Seleccionar duración'),
+            selectedTextStyle: TextStyle(color: Color(0xff16697a)),
+            onConfirm: (Picker picker, List<int> value) {
+              Duration _duration = Duration(minutes: picker.getSelectedValues()[0], seconds: picker.getSelectedValues()[1]);
+              setState(() {
+                if( ((_duration.inSeconds % 60) > 2) && ((_duration.inMinutes) == 0) ){
+                  widget.sec = _duration.inSeconds % 60;
+                  widget.min = _duration.inMinutes;
+                  widget.function(widget.sec,widget.min);
+                }else{
+                  final scaffold = Scaffold.of(context);
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: const Text('Valor invalido!'),
+                    ),
+                  );
+                }
+              });
+            },
+          ).showDialog(context);
+        },
+        color: Color(0xfff8f1f1), //color de time cards
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)
+          ),
+        textColor: Color(0xff16697a),
+        child:Container(
+          width: (MediaQuery.of(context).size.width)/1.25,
+          height: (MediaQuery.of(context).size.height)/13,
+          child: Row(
+            children: <Widget>[
+              Container(
+                  child:Icon(widget.cardIcon,
+                    size: 25,
+                  )
+              ),
+              Expanded(
+                flex: 1,
+                  child:Container(
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                )
-            ),
+              ),
+              Expanded(
+                flex: -1 ,
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        left: (MediaQuery.of(context).size.width)/11,
+                        right: (MediaQuery.of(context).size.width)/12
+                    ),
+                    child: Text(
+                      "${secToString(widget.min)}:${secToString(widget.sec)}",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+              ),
 
-        ]
-      )
-     )
+          ]
+        )
+       )
+      ),
     );
   }
 }
